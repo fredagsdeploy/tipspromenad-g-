7e881fd47a36d54f1b5bcf7940dffbc03adcfcec
André Samuelsson <andresamuelsson94@gmail.com>
2017-06-20 16:29:16 +0200

    Reduce question and answers to a sorted leaderboard and display it in ResultsView

--- client/ResultsView.js
+++ client/ResultsView.js
@@ -9,10 +9,9 @@ import {
 } from "react-native";
 import Button from "./Button";
 
-const Center = props => <View {...props} style={{ alignItems: "center" }} />;
+import _ from "lodash";
 
-const Alternative = ({ onChange, value }) =>
-  <TextInput style={styles.textInput} value={value} onChangeText={onChange} />;
+const Center = props => <View {...props} style={{ alignItems: "center" }} />;
 
 export default class ResultView extends React.Component {
   static navigationOptions = {
@@ -26,13 +25,63 @@ export default class ResultView extends React.Component {
 
   state = {};
 
+  generateLeaderboard = (questions, answers) => {
+    if (!answers) {
+      return [];
+    }
+
+    const addPointToUser = (acc, userId) => {
+      if (!(userId in acc)) {
+        acc[userId] = 0;
+      }
+      acc[userId] += 1;
+    };
+
+    const isCorrectAnswer = (question, userAnswer) => {
+      return question.alternatives[question.correctAlternative] === userAnswer;
+    };
+
+    return _.chain(questions)
+      .reduce((acc, question, questionId) => {
+        const currentQuestionUserAnswers = answers[question.id];
+        if (
+          question.correctAlternative === null ||
+          !currentQuestionUserAnswers
+        ) {
+          // Question cannot be processed. No correct alternative set or no one has answered it yet.
+          return acc;
+        }
+
+        for (let userId in currentQuestionUserAnswers) {
+          if (isCorrectAnswer(question, currentQuestionUserAnswers[userId])) {
+            addPointToUser(acc, userId);
+          }
+        }
+        return acc;
+      }, {})
+      .toPairs()
+      .sortBy(pair => pair[1])
+      .value();
+  };
+
   render() {
-    const { questions, answers } = this.props;
+    const { questions, answers } = this.props.screenProps;
+    const leaderBoard = this.generateLeaderboard(questions, answers);
 
     return (
       <ScrollView style={styles.container}>
         <Center>
           <Text style={styles.header}>Vem var görbra?</Text>
+          <View style={styles.scoreTableContainer}>
+            {leaderBoard.map((item, index) => {
+              return (
+                <View style={styles.scoreTableRow} key={index}>
+                  <Text style={styles.scoreTableNameCell}>{item[0]}</Text>
+                  <Text style={styles.scoreTablePointsCell}>{item[1]}</Text>
+                </View>
+              );
+            })}
+          </View>
 
         </Center>
       </ScrollView>
@@ -67,5 +116,19 @@ const styles = StyleSheet.create({
   error: {
     marginTop: 10,
     color: "tomato"
+  },
+  scoreTableContainer: {
+    flex: 1
+  },
+  scoreTableRow: {
+    flexDirection: "row"
+  },
+  scoreTableNameCell: {
+    fontSize: 26,
+    marginRight: 10
+  },
+  scoreTablePointsCell: {
+    fontSize: 26,
+    marginLeft: 10
   }
 });
